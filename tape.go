d39b1019e2245f714e7125392a277e2f545fc46e
Alex Nichol <unixpickle@gmail.com>
2017-04-16 17:40:10 -0400

    drafted Tape API
    This will be necessary for reinforcement learning, where we want to record the entire episode so that we can back-propagate through it. Ideally, a Tape could be stored anywhere (e.g. a disk) so that we are not horribly bounded by memory.

---
+++ tape.go
@@ -0,0 +1,101 @@
+package lazyrnn
+
+import (
+	"sync"
+
+	"github.com/unixpickle/anydiff/anyseq"
+)
+
+// A Tape is a non-differentiable sequence that can be
+// re-read arbitrarily and may be generated while it is
+// being read.
+//
+// Tapes can be used to store and re-use sequences.
+// For example, you can use a Tape to record the results
+// of a reinforcement learning episode as it happens.
+type Tape interface {
+	// ReadTape generates a channel that is sent a range
+	// of values from the tape.
+	//
+	// If end is -1, then the entire Tape is read.
+	//
+	// If the Tape is still being generated, the resulting
+	// channel may be sent new values from the Tape as they
+	// are written.
+	//
+	// If the range is out of bounds even after the Tape
+	// is finished being generated, then the out of bounds
+	// part of the range is ignored.
+	// In other words, the channel will be closed before
+	// (end-start) values have been sent.
+	ReadTape(start, end int) <-chan *anyseq.Batch
+}
+
+type referenceTape struct {
+	lock      sync.Mutex
+	timesteps []*anyseq.Batch
+	done      bool
+	nextWait  chan struct{}
+}
+
+// ReferenceTape creates a Tape that stores the outputs by
+// retaining references to all of the batches from every
+// time-step.
+//
+// It produces a Tape and a corresponding writer channel.
+// The caller can send to the channel to add time-steps to
+// the tape, and then close the channel to complete the
+// tape.
+//
+// The caller must close the write channel to free
+// resources associated with the Tape.
+func ReferenceTape() (Tape, chan<- *anyseq.Batch) {
+	res := &referenceTape{nextWait: make(chan struct{})}
+	inChan := make(chan *anyseq.Batch, 1)
+	go res.readInputs(inChan)
+	return res, inChan
+}
+
+func (r *referenceTape) ReadTape(start, end int) <-chan *anyseq.Batch {
+	if start < 0 {
+		panic("negative start index")
+	} else if end < start && end != -1 {
+		panic("invalid end index")
+	}
+
+	res := make(chan *anyseq.Batch, 1)
+	go func() {
+		defer close(res)
+		for i := start; i < end || end == -1; i++ {
+			r.lock.Lock()
+			for i >= len(r.timesteps) {
+				if r.done {
+					r.lock.Unlock()
+					return
+				}
+				waiter := r.nextWait
+				r.lock.Unlock()
+				<-waiter
+				r.lock.Lock()
+			}
+			item := r.timesteps[i]
+			r.lock.Unlock()
+			res <- item
+		}
+	}()
+	return res
+}
+
+func (r *referenceTape) readInputs(inChan <-chan *anyseq.Batch) {
+	for input := range inChan {
+		r.lock.Lock()
+		r.timesteps = append(r.timesteps, input)
+		close(r.nextWait)
+		r.nextWait = make(chan struct{})
+		r.lock.Unlock()
+	}
+	r.lock.Lock()
+	r.done = true
+	close(r.nextWait)
+	r.lock.Unlock()
+}
---
+++ tape_test.go
@@ -0,0 +1,70 @@
+package lazyrnn
+
+import (
+	"testing"
+	"time"
+
+	"github.com/unixpickle/anydiff/anyseq"
+)
+
+func TestReferenceTape(t *testing.T) {
+	tape, writer := ReferenceTape()
+	readers := []<-chan *anyseq.Batch{
+		tape.ReadTape(1, 3),
+		tape.ReadTape(2, 5),
+		tape.ReadTape(2, -1),
+		tape.ReadTape(0, 2),
+	}
+
+	batches := []*anyseq.Batch{
+		{Present: []bool{true, false, true}},
+		{Present: []bool{true, false, false}},
+		{Present: []bool{true, false, false}},
+		{Present: []bool{false, false, false}},
+	}
+	writer <- batches[0]
+
+	mustNotRead(t, readers[:3]...)
+	mustRead(t, batches[0], readers[3])
+
+	writer <- batches[1]
+	mustRead(t, batches[1], readers[0], readers[3])
+	mustNotRead(t, readers[1], readers[2])
+
+	writer <- batches[2]
+	mustRead(t, batches[2], readers[:3]...)
+	mustRead(t, nil, readers[3])
+
+	writer <- batches[3]
+	mustRead(t, batches[3], readers[1:3]...)
+	mustRead(t, nil, readers[0], readers[3])
+
+	close(writer)
+
+	mustRead(t, nil, readers...)
+}
+
+func mustNotRead(t *testing.T, chans ...<-chan *anyseq.Batch) {
+	time.Sleep(time.Millisecond * 50)
+	for _, ch := range chans {
+		select {
+		case <-ch:
+			t.Fatal("did not expect to see value")
+		default:
+		}
+	}
+}
+
+func mustRead(t *testing.T, expected *anyseq.Batch, chans ...<-chan *anyseq.Batch) {
+	for _, ch := range chans {
+		timeout := time.After(time.Second)
+		select {
+		case actual := <-ch:
+			if actual != expected {
+				t.Fatalf("expected %v but got %v", expected, actual)
+			}
+		case <-timeout:
+			t.Fatalf("expected to see value: %v", expected)
+		}
+	}
+}
