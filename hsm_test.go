340d3385838de1824e1366982e7ffcffaaf56f91
Alex Nichol <unixpickle@gmail.com>
2017-04-18 16:04:04 -0400

    added some HSM benchmarks

--- hsm_test.go
+++ hsm_test.go
@@ -4,8 +4,12 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/unixpickle/anydiff"
 	"github.com/unixpickle/anydiff/anyseq"
+	"github.com/unixpickle/anynet"
 	"github.com/unixpickle/anynet/anyrnn"
+	"github.com/unixpickle/anyvec"
+	"github.com/unixpickle/anyvec/anyvec32"
 	"github.com/unixpickle/anyvec/anyvec64"
 )
 
@@ -59,4 +63,105 @@ func TestRecursiveHSMEquiv(t *testing.T) {
 			}
 		}
 	}
+}
+
+func BenchmarkBPTT(b *testing.B) {
+	b.Run("Regular", func(b *testing.B) {
+		c := anyvec32.DefaultCreator{}
+		ins, ups, block := setupHSMBenchmark(c)
+
+		inSeq := Unlazify(TapeRereader(c, ins))
+		upstreamBatches := Unlazify(TapeRereader(c, ups)).Output()
+		grad := anydiff.NewGrad(anynet.AllParameters(block)...)
+
+		b.ResetTimer()
+
+		for i := 0; i < b.N; i++ {
+			out := anyrnn.Map(inSeq, block)
+			out.Propagate(upstreamBatches, grad)
+		}
+	})
+	b.Run("Streaming", func(b *testing.B) {
+		benchmarkLazy(b, func(r Rereader, b anyrnn.Block) Seq {
+			return Lazify(anyrnn.Map(Unlazify(r), b))
+		})
+	})
+}
+
+func BenchmarkFixedSqrtT(b *testing.B) {
+	b.Run("Regular", func(b *testing.B) {
+		benchmarkLazy(b, func(r Rereader, b anyrnn.Block) Seq {
+			return FixedHSM(16, false, r, b)
+		})
+	})
+	b.Run("Lazy", func(b *testing.B) {
+		benchmarkLazy(b, func(r Rereader, b anyrnn.Block) Seq {
+			return FixedHSM(16, true, r, b)
+		})
+	})
+}
+
+func BenchmarkRecursiveHSM(b *testing.B) {
+	b.Run("Regular", func(b *testing.B) {
+		benchmarkLazy(b, func(r Rereader, b anyrnn.Block) Seq {
+			return RecursiveHSM(128, 2, false, r, b)
+		})
+	})
+	b.Run("Lazy", func(b *testing.B) {
+		benchmarkLazy(b, func(r Rereader, b anyrnn.Block) Seq {
+			return RecursiveHSM(128, 2, true, r, b)
+		})
+	})
+}
+
+func benchmarkLazy(b *testing.B, f func(Rereader, anyrnn.Block) Seq) {
+	c := anyvec32.DefaultCreator{}
+	ins, ups, block := setupHSMBenchmark(c)
+	grad := anydiff.NewGrad(anynet.AllParameters(block)...)
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		out := f(TapeRereader(c, ins), block)
+		upstream := make(chan *anyseq.Batch, 1)
+		go func() {
+			for b := range ups.ReadTape(0, -1) {
+				upstream <- b
+			}
+			close(upstream)
+		}()
+		out.Propagate(upstream, NewGrad(grad))
+	}
+}
+
+func setupHSMBenchmark(c anyvec.Creator) (inputs, upstream Tape, block anyrnn.Block) {
+	const seqLen = 256
+	const batchSize = 8
+	const inSize = 64
+	const outSize = 64
+
+	block = anyrnn.NewLSTM(c, inSize, outSize)
+	inputs, inputsCh := ReferenceTape()
+	upstream, upstreamCh := ReferenceTape()
+
+	for i := 0; i < seqLen; i++ {
+		batch := &anyseq.Batch{
+			Present: make([]bool, batchSize),
+			Packed:  c.MakeVector(batchSize * inSize),
+		}
+		upBatch := &anyseq.Batch{
+			Present: make([]bool, batchSize),
+			Packed:  c.MakeVector(batchSize * inSize),
+		}
+		for i := range batch.Present {
+			batch.Present[i] = true
+			upBatch.Present[i] = true
+		}
+		inputsCh <- batch
+		upstreamCh <- upBatch
+	}
+
+	close(inputsCh)
+	close(upstreamCh)
+
+	return
 }
