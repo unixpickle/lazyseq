636ad4ae80a65196431e94000837330b9bd5391a
Alex Nichol <unixpickle@gmail.com>
2017-04-24 11:47:57 -0400

    added ReduceTape() method

---
+++ reduce.go
@@ -0,0 +1,44 @@
+package lazyseq
+
+import "github.com/unixpickle/anydiff/anyseq"
+
+type reducedTape struct {
+	In      Tape
+	Present []bool
+}
+
+// ReduceTape produces a Tape without the sequences at
+// indices where present is false.
+func ReduceTape(t Tape, present []bool) Tape {
+	return &reducedTape{In: t, Present: present}
+}
+
+func (r *reducedTape) ReadTape(start, end int) <-chan *anyseq.Batch {
+	res := make(chan *anyseq.Batch, 1)
+	go func() {
+		defer close(res)
+		for in := range r.In.ReadTape(start, end) {
+			subset := append([]bool{}, in.Present...)
+			changed := false
+			numPresent := 0
+			for i, mask := range r.Present {
+				if subset[i] {
+					if !mask {
+						changed = true
+						subset[i] = false
+					} else {
+						numPresent++
+					}
+				}
+			}
+			if numPresent == 0 {
+				break
+			} else if changed {
+				res <- in.Reduce(subset)
+			} else {
+				res <- in
+			}
+		}
+	}()
+	return res
+}
---
+++ test/reduce_test.go
@@ -0,0 +1,40 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/unixpickle/anydiff/anyseq"
+	"github.com/unixpickle/anyvec/anyvec64"
+	"github.com/unixpickle/lazyseq"
+)
+
+func TestReduceTape(t *testing.T) {
+	c := anyvec64.DefaultCreator{}
+
+	tape, writer := lazyseq.ReferenceTape()
+
+	writer <- &anyseq.Batch{
+		Packed:  c.MakeVectorData([]float64{1, 2, 3, 4, 5, 6}),
+		Present: []bool{true, false, true, true},
+	}
+	writer <- &anyseq.Batch{
+		Packed:  c.MakeVectorData([]float64{-1, -2, 7, 8}),
+		Present: []bool{false, false, true, true},
+	}
+	writer <- &anyseq.Batch{
+		Packed:  c.MakeVectorData([]float64{-3, -4}),
+		Present: []bool{false, false, true, false},
+	}
+	close(writer)
+
+	out := lazyseq.ReduceTape(tape, []bool{true, true, false, true}).ReadTape(0, -1)
+	mustRead(t, &anyseq.Batch{
+		Packed:  c.MakeVectorData([]float64{1, 2, 5, 6}),
+		Present: []bool{true, false, false, true},
+	}, out)
+	mustRead(t, &anyseq.Batch{
+		Packed:  c.MakeVectorData([]float64{7, 8}),
+		Present: []bool{false, false, false, true},
+	}, out)
+	mustRead(t, nil, out)
+}
